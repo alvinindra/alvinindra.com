5454aeec452dc69f98c71791be1907189aadec07
Alvin Indra Pratama <alvinindrap@gmail.com>
2025-10-18 20:14:39 +0700

    feat: update favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,11 +47,11 @@ export const metadata: Metadata = {
   keywords: SITE_INFO.keywords,
   authors: [
     {
-      name: "ncdai",
+      name: "alvinindra",
       url: SITE_INFO.url,
     },
   ],
-  creator: "ncdai",
+  creator: "alvinindra",
   openGraph: {
     siteName: SITE_INFO.name,
     url: "/",
@@ -71,22 +71,22 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    creator: "@iamncdai", // Twitter username
+    creator: "@alvinindrapra", // Twitter username
     images: [SITE_INFO.ogImage],
   },
   icons: {
     icon: [
       {
-        url: "https://assets.chanhdai.com/images/favicon.ico",
+        url: "/images/icons/favicon.svg",
         sizes: "any",
       },
       {
-        url: "https://assets.chanhdai.com/images/favicon.svg",
+        url: "/images/icons/favicon.svg",
         type: "image/svg+xml",
       },
     ],
     apple: {
-      url: "https://assets.chanhdai.com/images/apple-touch-icon.png",
+      url: "/images/apple-icon.png",
       type: "image/png",
       sizes: "180x180",
     },
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,6 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from "next"
 
-import { SITE_INFO } from "@/config/site";
+import { SITE_INFO } from "@/config/site"
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
@@ -9,28 +9,16 @@ export default function manifest(): MetadataRoute.Manifest {
     description: SITE_INFO.description,
     icons: [
       {
-        src: "https://assets.chanhdai.com/images/icon-vector.svg",
+        src: "/images/icons/favicon.svg",
         type: "image/svg+xml",
         sizes: "any",
         purpose: "any",
       },
       {
-        src: "https://assets.chanhdai.com/images/icon-192x192.png",
+        src: "/images/icons/android-icon-192x192.png",
         type: "image/png",
         sizes: "192x192",
         purpose: "any",
-      },
-      {
-        src: "https://assets.chanhdai.com/images/icon-512x512.png",
-        type: "image/png",
-        sizes: "512x512",
-        purpose: "any",
-      },
-      {
-        src: "https://assets.chanhdai.com/images/maskable-icon.png",
-        type: "image/png",
-        sizes: "512x512",
-        purpose: "maskable",
       },
     ],
     id: "/?utm_source=pwa",
@@ -39,29 +27,11 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: "/",
     screenshots: [
       {
-        src: "https://assets.chanhdai.com/images/screenshot-mobile-dark.webp",
-        type: "image/webp",
-        sizes: "440x956",
-        form_factor: "narrow",
-      },
-      {
-        src: "https://assets.chanhdai.com/images/screenshot-mobile-light.webp",
-        type: "image/webp",
-        sizes: "440x956",
-        form_factor: "narrow",
-      },
-      {
-        src: "https://assets.chanhdai.com/images/screenshot-desktop-dark.webp",
-        type: "image/webp",
-        sizes: "1920x1080",
-        form_factor: "wide",
-      },
-      {
-        src: "https://assets.chanhdai.com/images/screenshot-desktop-light.webp",
+        src: "/images/blog/capture-alvinindra.com.png",
         type: "image/webp",
         sizes: "1920x1080",
         form_factor: "wide",
       },
     ],
-  };
+  }
 }
