0b2371d98c4cfe79dc0c993efb22fa78dc8ce2b4
Alvin Indra Pratama <alvinindrap@gmail.com>
2026-05-29 01:38:27 +0700

    chore(deps): bump runtime major deps and migrate vcard-creator v1 api

--- src/app/vcard/route.ts
+++ src/app/vcard/route.ts
@@ -11,20 +11,20 @@ export async function GET() {
   const card = new VCard()
 
   card
-    .addName(USER.lastName, USER.firstName)
-    // .addPhoneNumber(decodePhoneNumber(USER.phoneNumber))
-    .addAddress(USER.address)
-    .addEmail(decodeEmail(USER.email))
-    .addURL(USER.website)
+    .addName({ familyName: USER.lastName, givenName: USER.firstName })
+    // .addPhoneNumber({ number: decodePhoneNumber(USER.phoneNumber) })
+    .addAddress({ country: USER.address })
+    .addEmail({ address: decodeEmail(USER.email) })
+    .addUrl({ url: USER.website })
 
   const photo = await getVCardPhoto(USER.avatar)
   if (photo) {
-    card.addPhoto(photo.image, photo.mine)
+    card.addPhoto({ image: photo.image, mime: photo.mime })
   }
 
   if (USER.jobs.length > 0) {
     const company = USER.jobs[0]
-    card.addCompany(company.company).addJobtitle(company.title)
+    card.addCompany({ name: company.company }).addJobtitle(company.title)
   }
 
   return new NextResponse(card.toString(), {
@@ -59,7 +59,7 @@ async function getVCardPhoto(url: string) {
 
     return {
       image,
-      mine: "jpeg",
+      mime: "jpeg",
     }
   } catch {
     return null
